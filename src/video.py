fc539790d150d1d8c1dc96c2483bf81ae6c683e4
Ildar Gabdullin <ildar8803@gmail.com>
2023-06-18 20:01:45 +0300

    Add fourth homework

---
+++ src/video.py
@@ -0,0 +1,22 @@
+from src.channel import youtube
+
+
+class Video:
+    def __init__(self, video_id):
+        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=video_id
+                                               ).execute()
+        self.video_id = video_id
+        self.url = f'https://youtu.be/{self.video_id}'
+        self.video_title = video_response['items'][0]['snippet']['title']
+        self.view_count = video_response['items'][0]['statistics']['viewCount']
+        self.like_count = video_response['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return self.video_title
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
